65387aaae98cb701748ebcae26c641ac2bcd3498
iz-aak <izaak33333@gmail.com>
2026-05-08 23:19:36 +0100

    v2

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+import { initNav } from "./nav.js";
+import { initHero } from "./hero.js";
+import { initProjects } from "./projects.js";
+import { initStats } from "./stats.js";
+import { initGuestbook } from "./guestbook.js";
+import { initGallery } from "./gallery.js";
+import { initFooter } from "./footer.js";
+
+initNav();
+initHero();
+initProjects();
+initStats();
+initGuestbook();
+initGallery();
+initFooter();
